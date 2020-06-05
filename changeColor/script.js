791df07f2316fb7c8d236d4068371b9b0fbb665d
jason <jason_doan94@yahoo.com>
2020-06-05 18:08:08 -0400

    hex color random

--- changeColor/script.js
+++ changeColor/script.js
@@ -1,12 +1,32 @@
-const colorBtn = document.querySelector(".colorBtn");
+// const colorBtn = document.querySelector(".colorBtn");
+// const bodyBcg = document.querySelector("body");
+
+// const colors = ["yellow", "red", "green", "#3b5998"];
+
+// colorBtn.addEventListener("click", changeColor);
+
+// function changeColor() {
+//   //bodyBcg.style.backgroundColor = colors[2];
+//   let random = Math.floor(Math.random() * colors.length);
+//   bodyBcg.style.backgroundColor = colors[random]
+// }
+
+const hexNumbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+const hexBtn = document.querySelector(".hexBtn");
 const bodyBcg = document.querySelector("body");
+const hex = document.querySelector(".hex");
 
-const colors = ["yellow", "red", "green", "#3b5998"];
+hexBtn.addEventListener("click", getHex);
 
-colorBtn.addEventListener("click", changeColor);
+function getHex() {
+  let hexCol = "#";
 
-function changeColor() {
-  //bodyBcg.style.backgroundColor = colors[2];
-  let random = Math.floor(Math.random() * colors.length);
-  bodyBcg.style.backgroundColor = colors[random];
+  for (var i = 0; i < 6; i++) {
+    let random = Math.floor(Math.random() * hexNumbers.length);
+    hexCol += hexNumbers[random];
+    // console.log(random);
+    // console.log(hexCol);
+    bodyBcg.style.backgroundColor = hexCol;
+    hex.innerHTML = hexCol;
+  }
 }
